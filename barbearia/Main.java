b65a8ecc890a2b5fd07bcec3e5e9d222a2a66e34
caionsantos <137624365+caionsantos@users.noreply.github.com>
2024-07-05 21:20:35 -0300

    fixes finais

--- barbearia/Main.java
+++ barbearia/Main.java
@@ -1,53 +1,57 @@
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.Random;
 
 class Barbearia {
     private final int cadeiras;
     private final Semaphore semCadeiras;
     private final Semaphore semBarbeiro;
     private final Lock lock = new ReentrantLock(true);
+    private boolean dormindo;
 
     public Barbearia(int cadeiras) {
         this.cadeiras = cadeiras;
         this.semCadeiras = new Semaphore(cadeiras);
-        this.semBarbeiro = new Semaphore(0);
+        this.semBarbeiro = new Semaphore(1);
+        this.dormindo = false;
     }
 
-    public void entraCliente(int clienteId) {
-        lock.lock();
-        try {
-            if (semCadeiras.tryAcquire()) {
-                System.out.println("Cliente " + clienteId + " sentou-se na sala de espera.");
-                semBarbeiro.release();
-                semCadeiras.acquire();
-            } else {
-                System.out.println("Cliente " + clienteId + " foi embora, não há cadeiras disponíveis.");
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
+    public void entraCliente(int clienteId) throws InterruptedException {
+        if (semCadeiras.availablePermits() == 10 && this.dormindo){
+            lock.lock();
+            this.dormindo = false;
+            System.out.println("Barbeiro foi acordado por " + clienteId);
             lock.unlock();
+            semCadeiras.acquire();
+            this.atendeCliente(clienteId);
+        }
+        else if (semCadeiras.tryAcquire()) {
+            System.out.println("Cliente " + clienteId + " sentou-se na sala de espera.");
+            this.atendeCliente(clienteId);
+        } else {
+            System.out.println("Cliente " + clienteId + " foi embora, não há cadeiras disponíveis.");
         }
     }
 
-    public void atendeCliente() {
-        while (true) {
-            try {
-                semBarbeiro.acquire();
-                lock.lock();
-                try {
-                    System.out.println("Barbeiro está atendendo um cliente.");
-                    Thread.sleep(2000);
-                    System.out.println("Barbeiro terminou de atender um cliente.");
-                    semCadeiras.release();
-                } finally {
-                    lock.unlock();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+    public void atendeCliente(int clienteId) throws InterruptedException {
+        semCadeiras.release();
+        semBarbeiro.acquire();
+        System.out.println("Barbeiro está atendendo o cliente " + clienteId);
+        Thread.sleep(2000);
+        System.out.println("Barbeiro terminou de atender o cliente " + clienteId);
+
+//        if (!semCadeiras.tryAcquire(10)) {
+//            System.out.println("Barbeiro foi dormir");
+//            lock.lock();
+//            this.dormindo = true;
+//            lock.unlock();
+//        }
+//        else {
+//            semCadeiras.release(10);
+//        }
+        this.dormindo = true;
+        semBarbeiro.release();
     }
 }
 
@@ -62,27 +66,28 @@ class Cliente implements Runnable {
 
     @Override
     public void run() {
+        try {
+            Thread.sleep(new Random().nextInt(100000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("Cliente " + clienteId + " chegou.");
-        barbearia.entraCliente(clienteId);
+        try {
+            barbearia.entraCliente(clienteId);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Barbearia barbearia = new Barbearia(5);
+        Barbearia barbearia = new Barbearia(10);
 
-        Thread barbeiro = new Thread(barbearia::atendeCliente);
-        barbeiro.start();
-
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i < 100; i++) {
             Cliente cliente = new Cliente(barbearia, i);
             Thread clienteThread = new Thread(cliente);
             clienteThread.start();
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
